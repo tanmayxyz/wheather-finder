08c7230101ce56e2a438e5809485f7433a15ceb6
tanmayxyz <tanmaymaityxyz@gmail.com>
2019-04-04 01:05:14 +0530

    added state
    added dynamic input for city and country
    
    api key properly working

--- src/App.js
+++ src/App.js
@@ -6,16 +6,34 @@ import Wheather from './components/Wheather';
 const API_KEY = "5c3b05d5417e9bfb0a7448ac299e3b89";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.getWheather = this.getWheather.bind(this);
+  
+  state = {
+    temperature : undefined,
+    city : undefined,
+    country : undefined,
+    humidity : undefined,
+    description : undefined,
+    error : undefined
   }
 
   getWheather = async (e) => {
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=kolkata,india&appid=${API_KEY}&units=metric`)
-    const json_data = await api_call.json();
-    console.log(json_data);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
+    const data = await api_call.json();
+    console.log(data);
+
+    this.setState({
+      temperature : data.main.temp,
+      city :data.name,
+      country : data.sys.country,
+      humidity : data.main.humidity,
+      description : data.weather[0].description,
+      error : undefined
+
+    })
   }
 
 
